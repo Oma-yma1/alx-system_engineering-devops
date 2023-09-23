2ec70a29930e5036f12ed188a3a694f67c0fb586
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-23 06:59:31 -0700

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Client configuration file (w/ Puppet) ssh
+file_line { 'Turn off passwd auth':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentification no',
+}
+file_line { 'Declare identity file':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentifyFile ~/.ssh/school',
+}
