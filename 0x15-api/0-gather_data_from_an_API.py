e4cff26162d93be98410bbb0b117ddcca02d6915
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-23 07:24:05 -0700

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
-"""Returns information about TODO list progress FOR A GIVEN EMP ID"""
+"""returns information about TODO list progress for a given emp id"""
 import requests
 import sys
 
 
-BASE_URL = "https://jsonplaceholder.typicode.com"
+def emp_data(id):
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    resp = requests.get(url)
+    resp_json = resp.json()
+    name = resp_json.get("name")
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todos = requests.get(url)
+    todos_json = todos.json()
+    num_task = len(todos_json)
+    compleated = 0
+    list = ""
+    for task in todos_json:
+        if task.get("completed") is True:
+            compleated += 1
+            list += "\t " + task.get("title") + "\n"
 
-
-def get_data(id):
-    try:
-        response = requests.get(f"{BASE_URL}/users/{id}")
-        data = response.json()
-        name = data.get("name")
-        todos_resp = requests.get(f"{BASE_URL}/todos?userId={id}")
-        todos = todos_resp.json()
-        completed_task = [task for task in todos if task["completed"]]
-        num_complet = len(completed_task)
-        total_task = len(todos)
-        return name, num_complet, total_task, completed_task
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data from the API:", e)
-        sys.exit(1)
-
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <id>")
-        sys.exit(1)
-    id = int(sys.argv[1])
-    name, num_complet, total_task, completed_task = get_data(id)
-    print(f"Employee {name} is done with tasks({num_complet}/{total_task}):")
-
-    for task in completed_task:
-        print(f"\t{task['title']}")
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          compleated,
+                                                          num_task))
+    print(list[:-1])
 
 
 if __name__ == "__main__":
-    main()
+    emp_data(sys.argv[1])
