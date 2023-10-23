d4968a477c52ee403d3958bd7f780ef71397b957
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-23 09:31:29 -0700

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""returns information about for a given employee ID"""
+""""""
 import requests
 import sys
 
-
-def emp_data(id):
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    resp = requests.get(url)
-    resp_json = resp.json()
-    name = resp_json.get("name")
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    todos = requests.get(url)
-    todos_json = todos.json()
-    num_task = len(todos_json)
-    compleeted = 0
-    list = ""
-    for task in todos_json:
-        if task.get("completed") is True:
-            compleeted += 1
-            list += "\t " + task.get("title") + "\n"
-    print("Employee {} is done with tasks({}/{}):".format(name,
-                                                          compleeted,
-                                                          num_task))
-    print(list[:-1])
-
-
 if __name__ == "__main__":
-    emp_data(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/"
+    user_json = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos_json = requests.get(
+            url + "todos",
+            params={"userId": sys.argv[1]}
+            ).json()
+    completed = [i.get("title") for i in
+                 todos_json if i.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_json.get("name"), len(completed), len(todos_json)))
+    [print("\t {}".format(j)) for j in completed]
