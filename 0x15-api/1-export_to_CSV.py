a719826a9b8191b4abecccb6aff790816370287e
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-23 09:45:13 -0700

    task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  """
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_json = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user_js = requests.get(url + "users/{}".format(user_json)).json()
+    username = user_js.get("username")
+    todos_json = requests.get(
+            url + "todos",
+            params={"userId": user_json}
+            ).json()
+    with open("{}.csv".format(user_json), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_json, username, i.get("completed"), i.get("title")]
+         ) for i in todos_json]
