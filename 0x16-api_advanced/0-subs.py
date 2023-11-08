58e8dc3a99580f5171871b68560b1e6b3fbcc3d4
Oma-yma1 <zgani.omayma@gmail.com>
2023-11-08 08:02:21 -0800

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ returns num of tot subscribers"""
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return (0)
+    response = response.json()
+    if 'data' in response:
+        return (response.get('data').get('subscribers'))
+
+    else:
+        return (0)
