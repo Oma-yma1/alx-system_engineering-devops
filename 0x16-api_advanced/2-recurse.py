5fc3fad4a49040fd0a3cae135f305d2dde113810
Oma-yma1 <zgani.omayma@gmail.com>
2023-11-08 08:24:44 -0800

    task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""return list containing the titles of all articles for given subreddit"""
+"""
+Recursive function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit. If no results are found
+for the given subreddit, the function should return None.
+"""
 import requests
 
 
 def recurse(subreddit, hot_list=[]):
-    """function recurse"""
+    """
+    return list containing the titles of all art
+    for a given subreddit
+    """
     if type(subreddit) is list:
         url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
         url = "{}&after={}".format(url, subreddit[1])
