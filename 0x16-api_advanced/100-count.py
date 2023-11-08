bbdb781898a253e08748a75ac504b917258e6dcc
Oma-yma1 <zgani.omayma@gmail.com>
2023-11-08 08:46:29 -0800

    task4

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,52 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of given Reddit subreddit"""
+"""Function to count words in all hot posts of a given Reddit subreddit"""
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
+def count_words(subreddit, word_list, after=None, counts=None):
+    """function count words"""
+    if counts is None:
+        counts = {}
+
+    if not subreddit:
         return
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    headers = {'User-agent': 'count_words_bot'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'after': after} if after else None
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        return
+
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+
+    for post in posts:
+        title = post['data']['title']
+        for word in word_list:
+            word = word.lower()
+            count = title.lower().count(word)
+            counts[word] = counts.get(word, 0) + count
+
+    if data['data']['after']:
+        return count_words(subreddit, word_list, data['data']['after'], counts)
+
+    result = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in result:
+        if count > 0:
+            print(f"{word}: {count}")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming
+              'python java javascript'".format(sys.argv[0]))
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        subreddit = sys.argv[1]
+        keywords = [x.lower() for x in sys.argv[2].split()]
+        count_words(subreddit, keywords)
