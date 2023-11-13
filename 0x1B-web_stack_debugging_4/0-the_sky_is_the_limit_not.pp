da3ffddae5df3ad0a571b2fb782e0cac6aceb3ec
Oma-yma1 <zgani.omayma@gmail.com>
2023-11-13 07:47:21 -0800

    task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#let’s fix our stack so that we get to 0 failed
+
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
+}
